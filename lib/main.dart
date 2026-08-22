606d3ff3cad43a93484bff8b17e7567e9156ed36
yahay9356-netizen <yahay9356@gmail.com>
2026-08-22 11:18:28 +0300

    New project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Islami());
+}
+class Islami extends StatelessWidget {
+  const Islami({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
