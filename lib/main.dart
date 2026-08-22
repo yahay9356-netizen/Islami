15a18f658df15316e8c83d1bb16e0a54175ac3bc
yahay9356-netizen <yahay9356@gmail.com>
2026-08-22 14:01:26 +0300

    spalch screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/sources/routs_manger.dart';
 
-void main(){
-  runApp(Islami());
+void main() {
+  runApp(const Islami());
 }
+
 class Islami extends StatelessWidget {
   const Islami({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      routes: RoutsManger.routs,
+      // initialRoute: RoutsManger.spalch,
     );
   }
 }
